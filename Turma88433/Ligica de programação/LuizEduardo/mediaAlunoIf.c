1acb151fb1f92173a893b7d0fce24c58cafc85f1
Luiz Eduardo De Jesus <86099664+Luiz-EUARDO1987@users.noreply.github.com>
2023-09-25 11:23:05 -0300

    Add files via upload

---
+++ Turma88433/Ligica de programação/LuizEduardo/mediaAlunoIf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //declaraçao de variaveis
+    float nota1,nota2,media;
+    //atribuiçao de variaveis pelo susario.entrada de dados
+    printf("Digite a primeira nota: \n");
+    scanf("%f",&nota1);
+    system("cls || clear");
+
+    printf("Digite a segunda nota: \n");
+    scanf("%f",&nota2);
+    system("cls || clear");
+    //opreçao aritimetica
+    media = (nota1+nota2)/2;
+    //saida de dados.exibindo os dados para o usuario.
+    printf("media: %.2f \n",media);
+    if (media >= 7)
+    {
+     printf("Aprovado");
+    }
+    else{
+        printf("Reprovado");
+    }
+}
