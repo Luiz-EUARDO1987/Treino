5361286d4132426eadb88cbc785869217ad1bf1d
luiz eduardo <luiz-eduardo1987@hotmail.com>
2023-10-21 10:41:16 -0300

    tudo rodando

--- Turma88433/Ligica de programação/LuizEduardo/WhileMediaAritimetica02.c
+++ Turma88433/Ligica de programação/LuizEduardo/WhileMediaAritimetica02.c
@@ -4,22 +4,22 @@
 
 int main()
 {
-    //declarações de variáveis
+    //declara??es de vari?veis
     int numero = 0 ;
     float media = 0;
     int i = 0;
     int contador = 0;
 
-    //estruturade repetições. variavel de saida menos um.
+    //estruturade repeti??es. variavel de saida menos um.
     do
     {
-        //atribuições de variavel numero.
+        //atribui??es de variavel numero.
         printf("Digite o numero %i\n",contador+1);
         scanf("%i", &numero);
         system("cls || clear");
         fflush(stdin);
 
-        //estrutura de decisão se. para ignorar valores menores que zero.
+        //estrutura de decis?o se. para ignorar valores menores que zero.
         if(numero > 0)
         {
             // opera de soma. atruindo os valores a media.
@@ -30,10 +30,10 @@ int main()
         
     }while(numero > -1);
 
-    //dividindo o numero de interações pela soma total dos numero.
+    //dividindo o numero de intera??es pela soma total dos numero.
     media /= contador;
 
-    //saida da operações.
+    //saida da opera??es.
     printf("media: %.2f\n",media);
 
     return 0;
---
+++ Turma88433/Ligica de programação/whileMediaInfinita.c
@@ -0,0 +1,95 @@
+/*programa pergunta se quer adicionar outra nota a media se digitar "N" sai do programa*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+#include <unistd.h>
+#define continue02 "C"
+#define sair "N"
+
+int main()
+{
+    char resposta[250] ="";
+    char escolhaCN = "";
+    int fim = 1;
+    float nota = 0;
+    float media = 0;
+    int contador = 0;
+    //int sim =1;
+    int nao =1;
+   
+    setlocale(LC_ALL,"portuguese");
+  //estrutura de repeti��o principal. do-while
+    do
+    {
+  
+      //menu de escolha adicionar notas a media ou sair
+        printf("----- Menu -----\nDigite uma das op��es abaixo\n (N) | para sair\n (C) | para inserir outra nota\n");
+        scanf(" %c", & escolhaCN);
+        system("cls || clear");
+        /*Para limpar o buffer em Windows, use: fflush(stdin)
+        Para limpar o buffer em Linux, use: __fpurge(stdin)*/
+      
+        // transformar letra em mai�sculas
+      escolhaCN =toupper(escolhaCN);
+      //comparando as strings e atribuindo o retorno para sim ou nao
+      switch (escolhaCN)
+      {
+      case 'C':
+
+         do
+         {
+          nota = 0;
+          printf("Digite A %i� nota\n",contador+1);
+          scanf("%f",&nota);
+          system("cls || clear");
+          // valida��o da notas
+          if(nota > 0 && nota <= 10)
+          {
+          contador++; 
+          fim =0;
+          media += nota;
+          }
+
+          }while(fim != 0);
+
+        break;
+
+      case 'N':
+
+        nao=0;
+        
+        break;
+      
+      default:
+
+        printf("Erro! escolha uma das op��es validas.\n");
+        sleep(5);
+        system("cls || clear");
+
+        break;
+      }
+      
+     
+    }while(nao != 0);
+
+    media /= contador;
+    
+    if(media > 7)
+    {
+      strcpy(resposta,"Aprovado");
+    }
+    else if(media >= 5)
+    {
+      strcpy(resposta,"Recupera��o");
+    }
+    else
+    {
+      strcpy(resposta,"Reprovado");
+    }
+  printf("media: %.1f\n Aluno: %s\n",media,resposta);
+    
+    return 0;
+}
